5266815354a6d5a13e516d09d288331ba2e80486
kelviy <52874397+kelviy@users.noreply.github.com>
2024-03-02 20:00:11 +0200

    implemented insert, find and getAllData methods

--- src/main/java/com/wxxkel001/genericapp/Application/BinaryTree/BinarySearchTree.java
+++ src/main/java/com/wxxkel001/genericapp/Application/BinaryTree/BinarySearchTree.java
@@ -1,43 +1,93 @@
 package com.wxxkel001.genericapp.Application.BinaryTree;
 
-public class BinarySearchTree<dataType> {
+import com.wxxkel001.genericapp.Application.GenericData;
+
+import java.util.ArrayList;
+
+public class BinarySearchTree<dataType extends GenericData> {
     private BinaryNode<dataType> root;
 
     public BinarySearchTree() {
         this.root = null;
     }
 
-    public BinaryNode<dataType> find() {
-        // TODO: search
-        return null;
-    }
-
-    public void delete() {
-        // TODO: delete
-    }
-
-    public boolean insert(dataType data) {
+    public BinaryNode<dataType> find(dataType d) {
         if (root == null) {
-            root = new BinaryNode<dataType>(data);
-            return true;
+            return null;
         }
         else {
-
+            return find(d, root);
         }
-        return false;
     }
 
-    public void inOrderTransversal() {
-        inOrderTransversal(root);
+    public BinaryNode<dataType> find(dataType d, BinaryNode<dataType> node) {
+        int cmp = d.compareTo(node.getData());
+
+        if (cmp == 0) {
+            return node;
+        }
+        else if (cmp < 0) {
+            return (node.getLeft() == null) ? null : find(d, node.getLeft());
+        }
+        else {
+            return (node.getRight() == null) ? null : find(d, node.getRight());
+        }
     }
 
-    public void inOrderTransversal(BinaryNode<dataType> node) {
+    public void insert(dataType data) {
+        if (root == null) {
+            root = new BinaryNode<dataType>(data);
+        }
+        else {
+            insert(d, root);
+        }
+    }
+
+
+    // TODO: How to handle duplicates
+    public void insert(dataType d, BinaryNode<dataType> node) {
+        if (d.compareTo(node.getData()) <= 0) {
+            if (node.getLeft() == null) {
+                node.setLeft(new BinaryNode<dataType>(d, null, null));
+            } else {
+                insert(d, node.getLeft());
+            }
+        }
+        else {
+            if (node.getRight() == null) {
+                node.setRight(new BinaryNode<dataType>(d, null, null));
+            } else {
+                insert(d, node.getRight());
+            }
+        }
+    }
+
+    public ArrayList<GenericData> getAllData() {
+        int size = getSize();
+        ArrayList<GenericData> data = new ArrayList<>();
+        getAllData(root, data);
+        return data;
+    }
+
+    private void getAllData(BinaryNode<dataType> node, ArrayList<GenericData> data) {
         if(node != null) {
-            // TODO: visitor action
-
             // convention of visiting the left side of the tree first
-            inOrderTransversal(node.getLeft());
-            inOrderTransversal(node.getRight());
+            getAllData(node.getLeft(), data);
+            data.add(node.getData());
+            getAllData(node.getRight(), data);
+        }
+    }
+
+    public int getSize() {
+        return getSize(root);
+    }
+
+    public int getSize(BinaryNode<dataType> node) {
+        if (node == null) {
+            return 0;
+        }
+        else {
+            return 1 + getSize(node.getLeft()) + getSize(node.getRight());
         }
     }
 
